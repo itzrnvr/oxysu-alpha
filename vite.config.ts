fe88eaeaafef465b15cb7539e14da192d4956272
Sanskar Singh <116519896+itzrnvr@users.noreply.github.com>
2025-07-03 07:52:51 +0530

    updated to have viteand routegen generated files to be at project roo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
+    tanstackRouter({
+      generatedRouteTree: 'routeTree.gen.ts',
+    }),
     react(),
   ],
 })
